03898263514f5c967bff2fe97a7a4d2a2af2ea24
Nishita222 <nishitaa2006@gmail.com>
2026-06-02 11:25:30 +0530

    Refactor practice1.java to include a loop

--- practice1.java
+++ practice1.java
@@ -4,14 +4,95 @@
 //         Scanner x = new Scanner (System.in);
 //         int age = x.nextInt();
 //         System.out.println(age);
+//         sc.close();
 //     } 
 // }
-import java.util.*;
-public class practice1{
-    public static void main (String[] args){
-        Scanner sc= new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println(name);
-    }
+// import java.util.*;
+// public class practice1{
+//     public static void main (String[] args){
+//         Scanner sc= new Scanner(System.in);
+//         String name = sc.nextLine();
+//         System.out.println(name);
+//         sc.close();
+//     }
     
+// }
+
+// import java.util.*;
+// public class practice1 {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.nextLine();
+//         int mf = sc.nextInt();
+//         switch(mf){
+//         case 1: System.out.println("male candidate");
+//         break;
+//         case 2: System.out.println("female candidate");
+//         break;
+//         default: System.out.println("invalid input");
+//         }
+//     }
+    
+// }
+
+public class practice1{
+    public static void main(String[] args){
+        for(int i =0 ;i<8;i++){
+
+            System.out.println("Apna college");
+        }
+    }
 }
+
+// import java.util.*;
+// public class practice1 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("choose between 1 or 0");
+//         int x = sc.nextInt();
+//         if (x==1){
+//             System.out.println("Enter marks of your subject"); 
+//             int y= sc.nextInt();
+//             if (y>=90) {
+//                 System.out.println("This is Good");
+//             }else if(y>60 ) {
+//                 System.out.println("This is also Good");
+//             }else{
+//                 System.out.println("This is Good as well");
+//             }             
+                      
+//         }  
+//         sc.close();  
+//     }
+// }
+
+// SOLID RHOMBUS
+// public class practice1 {
+//     public static void main(String[] args) {
+//         int n=5;
+//         int m=5;
+//         for (int i=n; i>=1;i--){
+//             for(int j=2; j<=i;j++){
+//                 System.out.print("/");
+//             }
+//             for (int j=1;j<=m;j++){
+//                 System.out.print("* ");
+//             }System.out.println();
+//         }
+//     }
+// }
+
+// public class practice1{
+//     public static void main(String[] args){
+//         int n = 5;
+//         int num = 1;
+//         for (int i=n;i>=1;i++){
+//             System.out.print(" ");
+//             for( int j=1 ; j<=i;j++){
+//                 System.out.print(num);
+//                 num++;
+//             }
+//         }System.out.println();
+//     }
+// }
